aeb1e3a84574cbf94fb4f9c6559d52e3a299ee11
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2022-03-18 19:27:38 -0300

    Created userCardsDisplay component and CustomLabels file

---
+++ force-app/main/default/lwc/userCardsDisplay/userCardsDisplay.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc'
+
+import ConhecaColegasEquipe from '@salesforce/label/c.ConhecaColegasEquipe'
+
+export default class UserCardsDisplay extends LightningElement {
+  labels = { ConhecaColegasEquipe }
+}
