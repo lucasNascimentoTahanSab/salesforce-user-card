d8c2f65dc556b8e18537fc81ac9c505d4402efd5
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2022-03-27 10:20:47 -0300

    Defined method responsible for getting user data on the client site using the wire api.

--- force-app/main/default/lwc/userCardsDisplay/userCardsDisplay.js
+++ force-app/main/default/lwc/userCardsDisplay/userCardsDisplay.js
@@ -1,7 +1,17 @@
-import { LightningElement } from 'lwc'
+import { LightningElement, wire } from 'lwc'
 
 import ConhecaColegasEquipe from '@salesforce/label/c.ConhecaColegasEquipe'
 
+import getUsersData from '@salesforce/apex/UserCardController.getUsersData'
+
 export default class UserCardsDisplay extends LightningElement {
   labels = { ConhecaColegasEquipe }
+  users = []
+
+  @wire(getUsersData)
+  getUserData(result) {
+    if (result.error) throw new Error(result.error)
+
+    this.users = JSON.parse(result.data)
+  }
 }
